aba63587e6da51e1205a7ddea6258d44b3a5fbea
p0lygun <solankivibhakar82@gmail.com>
2022-12-22 18:12:42 +0530

    feat: init repo

---
+++ website/website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-y67ov*m0363@y=(8-p*jyn)!nb*g*m=rm7a^lnwzg@ria4*8n9"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
