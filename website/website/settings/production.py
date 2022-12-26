6954c2b5cffadc10df48ac4f76a3bdc93038a499
p0lygun <solankivibhakar82@gmail.com>
2022-12-27 01:11:13 +0530

    feat: add CSRF_TRUSTED_ORIGINS

--- website/website/settings/production.py
+++ website/website/settings/production.py
@@ -8,6 +8,7 @@ SECRET_KEY = os.getenv('PRODUCTION_KEY')
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = json.loads(os.getenv('ALLOWED_HOSTS'))
+CSRF_TRUSTED_ORIGINS = json.loads(os.getenv('CSRF_TRUSTED_ORIGINS'))
 
 try:
     from .local import *
