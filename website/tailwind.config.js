48e166c92f16295672ee24a12fba04f6cffe4dfb
p0lygun <solankivibhakar82@gmail.com>
2023-03-10 12:55:30 +0530

    feat: add dark mode switcher button

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: 'class',
+    content: [
       "./**/templates/*.html",
       "./**/templates/**/*.html",
       "./**/static/*.js",
